b726935cc83a3b77770aacc817e903aabd663b58
Pooja Talaniya <pooja@FLEXI>
2024-03-05 11:32:22 -0500

    Initializing properties with values passed to the constructor.

--- MidtermPooja/Program.cs
+++ MidtermPooja/Program.cs
@@ -2,3 +2,24 @@
 //By Pooja Talaniya (8604569)-Ptalaniya4569@conestogac.on.ca
 
 //Beginning correction of the code
+
+using System;
+public class InventoryItem
+{
+    // Properties
+    public string ItemName { get; set; }
+    public int ItemId { get; set; }
+    public double Price { get; set; }
+    public int QuantityInStock { get; set; }
+
+    // Constructor
+    public InventoryItem(string itemName, int itemId, double price, int quantityInStock)
+    {
+        // Initializing the properties with the values passed to the constructor.
+        ItemName = itemName;
+        ItemId = itemId;
+        Price = price;
+        QuantityInStock = quantityInStock;
+    }
+
+
